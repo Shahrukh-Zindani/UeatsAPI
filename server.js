105ab290b1decbeb2e70d543b030fdd945e9aab9
Shahrukh Zindani <shahrukh.zindani@gmail.com>
2016-12-22 04:58:36 -0600

    Add first route to api

---
+++ server.js
@@ -0,0 +1,37 @@
+const cheerio = require('cheerio');
+const express = require('express');
+const request = require('request');
+
+var app = express();
+
+app.get('/schools', function(req, res) {
+	
+	var url = 'http://universityeats.com/';
+
+	request(url, (error, response, html) => {
+		if(!error && response.statusCode == 200) {
+
+			var $ = cheerio.load(html);
+			var json = {};
+
+			var numberOfSchools = $('.title').get().length;
+
+			for(var i = 0; i < numberOfSchools; i++) {
+				var row = $('.title').get(i);
+				name = $(row).text();
+				var restaurants = $('.hours').get(i);
+				restaurants = $(restaurants).text();
+				restaurants = Number(restaurants.split(' ')[0]);
+				json[name] = restaurants;
+			}
+			
+			res.send(json);	
+		}	
+	});
+});
+
+app.listen(3000, () => {
+	console.log("Running on 3000");
+});
+
+
